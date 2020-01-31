eeb9d96fe377a475c40ec3b5c2e920c9cc17c857
Gabriel Pérez <gabriel@garox.org>
2020-01-31 13:16:46 -0300

    remove reflector

--- src/helpers/decorators/graphql-user.decorator.ts
+++ src/helpers/decorators/graphql-user.decorator.ts
@@ -1,9 +1,6 @@
 import { createParamDecorator } from '@nestjs/common'
-import { Reflector } from '@nestjs/core'
 import { Roles } from 'src/app.roles'
 import { CAN_DO_ANY_SYMBOL, WANT_TO_KNOW_IF_CAN_DO_ANY } from '../constants'
-
-const reflector = new Reflector()
 
 /**
  * Returns a callback function that returns whether or not this user can perform [action] to any document
@@ -15,7 +12,7 @@ export const CanDoAny = (): ParameterDecorator => {
   return (target, key, index): void => {
     Reflect.defineMetadata(WANT_TO_KNOW_IF_CAN_DO_ANY, true, target[key])
     createParamDecorator(() => {
-      return (): Roles[] => reflector.get(CAN_DO_ANY_SYMBOL, target[key])
+      return (): Roles[] => Reflect.getMetadata(CAN_DO_ANY_SYMBOL, target[key])
     })()(target, key, index)
   }
 }
