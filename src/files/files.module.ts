de838eaa2858967e7c396238d4bea940966d69b3
Gabriel Pérez <gabriel@garox.org>
2020-02-12 13:03:03 -0300

    add file upload

---
+++ src/files/files.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common'
+import { FilesService } from './files.service'
+import { FilesResolver } from './files.resolver'
+
+@Module({
+  providers: [FilesService, FilesResolver],
+})
+export class FilesModule {}
