6323b166b7166db5acd0732951e725b32ce403e7
glabrat <german.labra@drimo.com>
2023-04-14 13:12:02 -0400

    fix: add support for eslint and prettier

--- src/orders/orders.input.ts
+++ src/orders/orders.input.ts
@@ -1,10 +1,10 @@
-import { InputType, OmitType, PartialType } from '@nestjs/graphql';
+import { InputType, OmitType, PartialType } from '@nestjs/graphql'
 
 @InputType()
 export class OrderInput {
-  itemId: string;
-  userId: string;
-  quantity: number;
+  itemId: string
+  userId: string
+  quantity: number
 }
 
 @InputType()
