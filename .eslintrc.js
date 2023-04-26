33bb6d8f2ff5eb230c2360678f6ef518f7a14005
glabrat <german.labra@drimo.com>
2023-04-26 17:10:53 -0400

    fix: node fetch for docker

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
     'max-params': 'off',
   },
 }
