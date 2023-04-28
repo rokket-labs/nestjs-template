9c11c1b8ba76db75ccf824dbb1abae6b1ff65d24
glabrat <german.labra@drimo.com>
2023-04-28 11:37:00 -0400

    fix: codefactor lint

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,8 +1,8 @@
-import { UseGuards } from '@nestjs/common'
+// import { UseGuards } from '@nestjs/common'
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 
-import { GqlAuthGuard } from 'src/auth/gql-auth.guard'
-import { IdTokenUser } from 'src/auth/jwt.strategy'
+// import { GqlAuthGuard } from 'src/auth/gql-auth.guard'
+// import { IdTokenUser } from 'src/auth/jwt.strategy'
 import { CurrentUser } from 'src/utils/decorators/current-user'
 
 import { UserDto } from './dto/user.dto'
